f81360ced71116b5b35cc2ab1f42a199a29c4aae
David J <97040451+DavidJ-WestMedia@users.noreply.github.com>
2022-08-19 18:05:35 +0200

    updaye

--- Game LIfe Counter/index.js
+++ Game LIfe Counter/index.js
@@ -1,35 +1,15 @@
 const LifeP = document.getElementById("life-p");
-const LifeP2 = document.getElementById("life-p2");
 const buttonChoice = document.querySelectorAll("button");
 let lifeTotal = 20 ;
 
 
-// buttonChoice.forEach(buttonC => buttonC.addEventListener("click", (e) => {
-//    if (e.target.id === "plus-btn") {
-//       lifeTotal += 1
-//    LifeP.textContent = lifeTotal
-//       lifeTotal.textContent += 1
-//    } else if(e.target.id === "minus-btn"){
-//       lifeTotal -= 1
-//       LifeP.textContent = lifeTotal
-//    }
-// }))
-
-buttonChoice.forEach(buttonC => buttonC.addEventListener("click", (e) => {
-      if (e.target.id === "plus-btn") {
-         lifeTotal += 1
-      LifeP.textContent = lifeTotal
-         lifeTotal.textContent += 1
-   }  else if(e.target.id === "minus-btn"){
-         lifeTotal -= 1
-         LifeP.textContent = lifeTotal
-         // second section
-   }else if (e.target.id === "plus-btn2") {
-         lifeTotal += 1
-      LifeP2.textContent = lifeTotal
-         lifeTotal.textContent += 1
-   }  else if(e.target.id === "minus-btn2"){
-         lifeTotal -= 1
-         LifeP2.textContent = lifeTotal
-   }
-}))
+ buttonChoice.forEach(buttonC => buttonC.addEventListener("click", (e) => {
+    if (e.target.id === "plus-btn") {
+       lifeTotal += 1
+    LifeP.textContent = lifeTotal
+       lifeTotal.textContent += 1
+    } else if(e.target.id === "minus-btn"){
+       lifeTotal -= 1
+       LifeP.textContent = lifeTotal
+    }
+ }))
